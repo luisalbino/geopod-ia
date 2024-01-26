0959e3c74754b14265622efc3f8a019aa4970a98
start.vaadin.com <no-reply@vaadin.com>
2024-01-26 00:44:06 +0000

    Generated project

---
+++ src/main/java/com/application/Application.java
@@ -0,0 +1,25 @@
+package com.application;
+
+import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The entry point of the Spring Boot application.
+ *
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ *
+ */
+@SpringBootApplication
+@NpmPackage(value = "@fontsource/quicksand", version = "4.5.0")
+@Theme(value = "geopod-ia")
+public class Application implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
