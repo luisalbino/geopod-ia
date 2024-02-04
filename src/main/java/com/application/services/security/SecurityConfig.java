f02242ffec56332e03050131fb185ded7bf5ab49
Luis Henrique Albino <luis@geovendas.com>
2024-02-04 18:46:35 -0300

    Iniciado lógica de login

---
+++ src/main/java/com/application/services/security/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.application.services.security;
+
+import com.application.views.user.LoginView;
+import com.vaadin.flow.spring.security.VaadinWebSecurity;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends VaadinWebSecurity {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth ->
+                auth.requestMatchers(
+                        AntPathRequestMatcher.antMatcher(HttpMethod.GET, "/images/*.png")).permitAll());
+        super.configure(http);
+        setLoginView(http, LoginView.class);
+    }
+
+    @Bean
+    public UserDetailsService users() {
+        return new InMemoryUserDetailsManager(User.builder()
+                .username("luis")
+                .password("{bcrypt}" + "$2a$12$8a10ACTmv/i9TPSW8q/rM.S/Q6m0H47FIO1qsjpb27xddVdamDAn.")
+                .roles("MASTER")
+                .build());
+    }
+}
+
