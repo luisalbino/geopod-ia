63cf11b0982b4f87a7026e2746393bb42a97f4f5
geovendas-jean <jean@geovendas.com>
2024-02-22 18:10:35 -0300

    wip token

---
+++ src/main/java/com/application/models/UserModel.java
@@ -0,0 +1,14 @@
+package com.application.models;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class UserModel {
+
+    private String username;
+
+    private String password;
+}
