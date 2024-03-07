6055c967ce1ef79fa754c405d86431b841987cb9
Luis Henrique Albino <luis@geovendas.com>
2024-03-07 18:45:34 -0300

    Criado notificationHelper e ajustado mensagem de erro ao excluir registro

--- src/main/java/com/application/components/importador/AbstractConsulta.java
+++ src/main/java/com/application/components/importador/AbstractConsulta.java
@@ -2,6 +2,7 @@ package com.application.components.importador;
 
 import com.application.components.customizado.CustomConfirmDialog;
 import com.application.entities.importador.AbstractImportadorEntity;
+import com.application.helpers.NotificationHelper;
 import com.application.helpers.interfaces.RunnableWithParameter;
 import com.application.services.AbstractService;
 import com.vaadin.flow.component.Component;
@@ -12,6 +13,7 @@ import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import org.springframework.dao.DataIntegrityViolationException;
 
 import java.util.Collection;
 
@@ -29,9 +31,19 @@ public abstract class AbstractConsulta<S extends AbstractService<E, ?>, E extend
         this.service = service;
         this.onEditarItem = onEditarItem;
         this.dados = this.service.getAll();
-        this.confirmaExclusao = new CustomConfirmDialog<E>("Excluir!", "Deseja realmente excluir o registro?", entidade -> {
-            service.remove(entidade);
-            atualizarDados();
+        this.confirmaExclusao = new CustomConfirmDialog<>("Excluir!", "Deseja realmente excluir o registro?", entidade -> {
+            try {
+                service.remove(entidade);
+                atualizarDados();
+                NotificationHelper.success("Registro removido com sucesso!");
+            } catch (DataIntegrityViolationException ve) {
+                var mensagemErro = ve.getMessage();
+                if (mensagemErro.contains("violates foreign key constraint")) {
+                    mensagemErro = "Esse registro está ligado a outra tabela!";
+                }
+
+                NotificationHelper.error(mensagemErro);
+            }
         });
 
         configFiltros();
