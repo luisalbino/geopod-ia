91e05a1e8db0d59f6667dfc94ef4c5cc54e6e8d7
Luis Henrique Albino <luis@geovendas.com>
2024-03-07 18:13:01 -0300

    Adicionado constraints para evitar registros incorretos

--- src/main/java/com/application/entities/importador/GeoScriptEntity.java
+++ src/main/java/com/application/entities/importador/GeoScriptEntity.java
@@ -2,15 +2,18 @@ package com.application.entities.importador;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Table(uniqueConstraints={@UniqueConstraint(name ="geoscript_uk", columnNames = {"scriptCode", "scriptModuleName", "perfil_id"})})
 @Entity(name = "geo_script")
 public class GeoScriptEntity extends AbstractImportadorEntity {
 
-    @ManyToOne
+    @ManyToOne(optional = false)
     private PerfilEntity perfil;
 
     private String scriptModuleName;
