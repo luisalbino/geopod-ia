f69e304c3956fe107acc55c4598d9b21ef31c667
Luis Henrique Albino <luis@geovendas.com>
2024-03-07 19:53:58 -0300

    Removido classe não usada

--- src/main/java/com/application/controllers/AuthController.java
+++ src/main/java/com/application/controllers/AuthController.java
@@ -3,12 +3,12 @@ package com.application.controllers;
 import com.application.helpers.JwtHelper;
 import com.application.models.UserModel;
 import com.application.models.controller.TokenResponseModel;
-import com.application.services.auth.AuthService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api")
 @RestController
