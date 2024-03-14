445c4dfec22dfd4cecdba1e24743679a5a47264c
geovendas-jean <jean@geovendas.com>
2024-03-14 18:35:31 -0300

    adicionado handler para exceções não tratadas

---
+++ src/main/java/com/application/handlers/RestExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.application.handlers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Ocorreu um erro inexperado.");
+    }
+}
